817370c26d12f29497b991dcab8a5780a97d9438
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-19 20:07:59 +0300

    Add average

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -2,15 +2,19 @@
   (:use training-day
         midje.sweet))
 
-(facts
+(facts "square"
   (square 2) => 4
   (square 3) => 9)
 
-(facts
+(facts "abs"
   (abs -2) => 2
   (abs 42) => 42)
 
-(facts
+(facts "fizzbuzz"
   (fizzbuzz 45) => "gotcha!"
   (fizzbuzz 48) => "fizz"
   (fizzbuzz 70) => "buzz")
+
+(facts "average"
+  (average 2 4) => 3
+  (average 1 2) => 3/2)
