4caa294c66249893f20318f7ec39d0eba897d91f
Ilmari Vacklin <ilmari.vacklin@eficode.com>
2012-05-19 20:07:09 +0300

    Update the Midje dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject training-day/training-day "1.0.0-SNAPSHOT" 
+(defproject training-day/training-day "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev
              {:dependencies
-              [[midje "1.3.1"]
+              [[midje "1.4.0"]
                [com.stuartsierra/lazytest "1.2.3"]]}}
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
