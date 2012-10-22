b5b170fe4e8ddedb75438ec74f301ec04fe38b58
jezberg <jerx@welho.com>
2012-10-22 12:05:40 +0300

    All excercises done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn 
+  square 
+  "Multiplies a number by itself"
+  [x] 
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn 
+  average
+  "Returns the average of it's parameters"
+  [a b]
+  (/ (+ a b) 2)
+)
