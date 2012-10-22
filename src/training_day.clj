9a5bad747b55f4e8b24375ab378d345d35c03b98
Deni Seitz <seitz@melkinpaasi.cs.helsinki.fi>
2012-10-22 20:37:43 +0300

    Eka setti

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
+(def ei-siis-hai "Ei siis hai!")
+
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
