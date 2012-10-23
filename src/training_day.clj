ee30331b54dc40ab80ed8fe10556200070608020
Janne Inkinen <inkinen.janne@gmail.com>
2012-10-23 14:01:20 +0300

    Ekan palautuksen toka yritys

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,24 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
+
+; (def answer ":(")
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square
+        "Korottaa toiseen potenssiin"
+        [x]
+        (* x x))
 
-(defn average [a b]
-  ":(")
+;(defn square [x]
+;  ":(")
+
+(defn average
+        "Ottaa keskiarvon"
+        [a b]
+        (/ (+ a b) 2))
+
+;(defn average [a b]
+;  ":(")
+
