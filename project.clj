f0fd7bd5fe1ffddb4bb9f747e713060543a31a13
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-31 17:52:15 +0300

    travis

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev
-             {:dependencies
-              [[midje "1.5-RC1"]]}}
-  :plugins [[lein-midje "3.0-RC1"]])
+             {:dependencies [[midje "1.5.1"]]
+              :plugins [[lein-midje "3.0.0"]]}})
