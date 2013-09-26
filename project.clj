8babc6d45c92172bffefbe982be5f6a0c1d75f5b
Jani Rahkola <jani.rahkola@iki.fi>
2013-09-26 14:31:22 +0300

    midje-grader as a dependecy

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [midje-grader "0.1.0-SNAPSHOT"]]
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]
               :plugins [[lein-midje "3.0.0"]]}})
