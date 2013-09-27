087e1f8c90d8ea49b53d140ea07cecd478b09451
Miika Sirén <mssiren.cs.helsinki.fi>
2013-09-27 15:06:22 +0300

    First exercises done

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
              {:dependencies [[midje "1.5.1"]]
               :plugins [[lein-midje "3.0.0"]]}})
