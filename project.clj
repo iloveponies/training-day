c551e20cb073a490ca7f4f6e2cd7fb46494c3812
niko.desktop <niko.ryytty@gmail.com>
2013-10-09 21:25:56 +0300

    Training day done

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev
-             {:dependencies [[midje "1.5.1"]]
+             {:dependencies [
+			[midje "1.5.1"]
+			[org.clojure/math.numeric-tower "0.0.2"]
+			    ]
               :plugins [[lein-midje "3.0.0"]]}})
--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
-(ns training-day)
+(ns training-day
+    (:require [clojure.math.numeric-tower :as math]))
 
-(def answer ":(")
+(def answer 
+    42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+    (math/expt x 2))
 
 (defn average [a b]
-  ":(")
+    (/ (+ a b) 2))
