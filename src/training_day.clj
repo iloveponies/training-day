6783eb403fe9f1372a6fdf20e746d2952dfa646d
Samppalol <sami.salonen@yahoo.com>
2013-10-19 23:55:59 +0300

    all done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
+(+(* 2 3) 4 )
+(+ 3 4 5 6)
+(get "abrakadabra" 0)
+((fn [name] (str "Welcome to Rivendell mr " name)) "Sami")
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/(+ a b) 2))
+
