8ab32308ed5932f8b05be0388159c44c58005ec4
nakkje <niklas.lillqvist@helsinki.fi>
2013-10-21 00:02:51 +0300

    exercise 1-7

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,19 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
-(def hai "O HAI!")
+(def hai "O AasdfdsffdsfasdHAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b)2))
+
+
+
+
+
+hai
+hai
+hai
