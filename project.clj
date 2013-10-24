e2bb5d45ea2ff7d2c03ab6c04f0ff1c9bd68bce4
Taxus <sami.lunnamo@cs.helsinki.fi>
2013-10-24 14:43:49 +0300

    Revert "jawesome"
    This reverts commit fbe44a25e59bb0f8ddf6ac741beb917cf31b30f9.
    
    Conflicts:
    	project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject training-day "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev
+             {:dependencies [[midje "1.5.1"]]
+              :plugins [[lein-midje "3.0.0"]]}})
