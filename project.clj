208aca72669a2dd82c35c872c9017c4ad3f64052
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-07 19:42:20 +0200

    Tests as a separate project

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje-grader "0.1.0-SNAPSHOT"]]
-  :profiles {:dev
-             {:dependencies [[midje "1.5.1"]]
-              :plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
