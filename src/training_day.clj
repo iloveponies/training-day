bba138be1b6e9a3c48de5425211cb659ed9e661a
Markus Moisio <markus.j.moisio@gmail.com>
2014-02-07 15:50:58 +0200

    Finished exercises

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer (int 42))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] "Calculates the suqare of x"
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average [a b] "Calculates the average of a and b"
+  (/ (+ a b) 2))
