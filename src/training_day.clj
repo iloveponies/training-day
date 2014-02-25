2d89dd0113dcb7875cb625117b38b4d9142875fd
Alistair Smith <alistairsmith@flightofstairs.org>
2014-02-25 22:15:29 +0000

    Tests pass.

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(get "Clojure" 2)
+
+(defn hello [who] (str "Hello, " who "!"))
+
+(hello "who")
+
