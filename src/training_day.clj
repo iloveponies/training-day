122434920b8d3bfa80a30754c39b480d95e10e50
bvfeld <sampo.virkkula@gmail.com>
2014-03-29 09:54:02 +0200

    first try

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+
+(square 3)
+(average 2 4)
+(average 1 2)
+
+
+
