2d4fd725722fea7765dadaa092506dc0497a1ca6
Julius Uusinarkaus <julius.uusinarkaus@gmail.com>
2014-04-01 18:21:52 +0300

    done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,21 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  [x]
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn
+  average
+  [a b]
+  (/ (+ a b) 2))
+
+(square 3)
+
+(average 2 4)
+
+
