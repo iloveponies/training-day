769c07aac1f1c836b4c3662091d0e1c9fe25ce6a
Yu Shen <yubrshen@gmail.com>
2014-04-05 03:23:49 -0500

    submit exercisez for training-day. All checks succeeded.

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
-(ns training-day)
+(ns training-day) ; declare the name space of training-day
+; See how the namespace is called training-day, but the file is training_day.clj?
+; This is intentional. If a namespace name has an hyphen, the corresponding file name should have an underscore.
 
-(def answer ":(")
+; (use 'training-day) to import the namespace to access the names in the space
+; ' is the short for (quote ) special form to prevent evaluation of the symbol being quoted.
+
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
