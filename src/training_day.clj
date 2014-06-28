9491c81fa176b685980fa07914197ed99af781e7
Paul Dumais <paul@unstate.ca>
2014-06-28 17:21:13 -0600

    first commit

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,22 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(comment
+  (square 7)
+  (+ (* 2 3) 4)
+  (+ 3 4 5 6)
+  *
+  (first "abrakadabra")
+  ((fn [name] (str "Welcome to Rivendell mr " name)) "Paul")
+  (user/clojuredocs min)
+  hai
+  )
