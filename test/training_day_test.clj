91d5e49a6a536f0787f1d773b247313a11c1e0ad
vlad chub <vlad.chub@gmail.com>
2014-06-30 13:50:26 +0600

    implement ex. func

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,2 +1,14 @@
 (ns training-day-test
-  (:use iloveponies.tests.training-day))
+  (:use iloveponies.tests.training-day midje.sweet training-day))
+
+  (facts "square" {:exercise 6
+                  :points 1}
+    (square 2) => 4
+    (square 1) => 1
+    (square 3) => 9)
+
+  (fact "average"
+    (average 3 4) => 7/2)
+
+
+
