10d4860e4979de2503a174f426bd32dd3b1f52ba
HeavenlyAwe <christoffer.fridlund@hotmail.com>
2014-09-02 19:21:45 +0300

    Made an Eclipse project from the Leiningen project.

--- src/training_day.clj
+++ src/training_day.clj
@@ -5,7 +5,14 @@
 (def hai "O HAI!")
 
 (defn square [x]
-  (* x x))
+	(* x x))
 
 (defn average [a b]
-  (/ (+ a b) 2))
+	(/ (+ a b) 2))
+
+
+(defn
+	hello
+	"Gives out personalized greetings."
+	[who]
+	(str "Hello, " who "!"))
