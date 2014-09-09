dcac087172e725d2dfebcea517254d9a7059db57
Roni Kekkonen <roni.kekkonen@gmail.com>
2014-09-09 11:51:52 +0300

    Completed 9.9.2914

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,12 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Calculates x^2"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
