7e00184df9c94b86331c32ce30d52166a07cdebc
unknown <latham.fell@LP11-T0008.B2S.local>
2014-09-09 10:51:40 -0700

    first push

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Takes x and multiplies it by itself"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "Returns the average of a and b as a ratio"
+  (/ (+ a b) 2))
