060352a0e99bdcaaea1a4b104225c963cabde5c2
Joonatan Vuorela <joonatan.vuorela@cs.helsinki.fi>
2014-11-14 17:24:26 +0200

    just commiting first exercises

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,12 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [n]
+  (* n n))
 
 (defn average [a b]
-  ":(")
+  (/(+ a b) 2))
+
