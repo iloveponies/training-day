5661d5b8e4bf510e1999da65246920f71c35c57e
Gopal Ramachandran <gsebastianr@gmail.com>
2014-11-20 14:21:22 -0500

    first pset, first attempt

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,15 @@
-(ns training-day)
+(ns training-day) ;; namespace definition
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "computes the square of two numbers"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "computes the average of two numbers
+  (/ (+ a b) 2))
+
+
