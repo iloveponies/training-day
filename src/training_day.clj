0eb8cf45e1e8578a35ec1143068e4a5b7f86a49d
Gopal Ramachandran <gsebastianr@gmail.com>
2014-11-22 21:49:37 -0500

    first pset, second attempt, omments removed

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,15 +1,13 @@
-(ns training-day) ;; namespace definition
+(ns training-day)
 
 (def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  "computes the square of two numbers"
   (* x x))
 
 (defn average [a b]
-  "computes the average of two numbers
   (/ (+ a b) 2))
 
 
