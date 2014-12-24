6bdbc6c0e7e275f910fdfad967f4419fcc764337
Magnus Grimstvedt Saltnes <magnussaltnes@gmail.com>
2014-12-25 00:48:09 +0100

    Got this one working.

--- src/glommed-indexes.clj
+++ src/glommed-indexes.clj
@@ -5,26 +5,17 @@
   (:require [criterium.core :refer [bench]]
             [clojure.core.reducers :refer [fold]]))
 
-(defn indexes "Transducer finding indexes of an element occuring in a sequence"
+(defn indices "Transducer finding indices of an element occuring in a sequence"
   [element]
   (keep-indexed #(when (= element %2) %1)))
 
-(defonce long-string (clojure.string/join (repeatedly 10000 (partial rand-nth [\1 \0])) ))
-
-
-(fold plus combine  (eduction (comp (indexes \1) (map #(vector % %))) long-string))
-
-(plus [[[0 0]] [2 3]] [5 5])
-
-(defn combine [[accepted-1 boundary-1] [accepted-2 boundary-2]]
-  (let[[[a b] [c d]] [boundary-1 (first accepted-2)]]
-    (if (<= b c (inc b))
-      [(into (if (empty? accepted-1) [] (pop accepted-1)) (cons [a d] accepted-2))  boundary-2]
-      [(into accepted-1 (cons boundary-1 accepted-2)) boundary-2])))
-
-
-(concat (drop-last [[0 3] [4 4]])  [[5 10]] [[12 13] [16 16]])
-(combine [[] [0 0]] [[[1 3]] [9 10]])
+(defn combine
+  ([] [[] nil])
+  ([[acc-a bnd-a] [acc-b bnd-b]]
+   (let[ [[a b] [c d]] [bnd-a (first acc-b)] ]
+     (if (<= b c (inc b))
+       [(into acc-a (concat [[a d]] (pop acc-b) )) bnd-b]
+       [(into acc-a (concat [bnd-a] acc-b)) bnd-b]))))
 
 (defn plus
   ([] [[] nil])
@@ -35,3 +26,8 @@
       (if (<= b c (inc b))
         [accepted [a d]]
         [(conj accepted boundary) to-add])))))
+
+(defn printable-indices [element the-seq]
+  (let[glommed (fold combine plus (eduction (comp (indexes \1) (map #(vector % %))) the-seq))
+       fixed-up (conj (first glommed) (last glommed))]
+    (clojure.string/join ", " (map (fn [[a b]](if (= a b) (str a) (str a \- b)))) fixed-up)))
