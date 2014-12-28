61720d28f96eb8392e3b7c7dc0fd705c0e8bba28
aozkan <armagan.ozkan@gmail.com>
2014-12-27 22:44:14 -0500

    Answers - Chapter 1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,33 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
 
-(defn average [a b]
-  ":(")
+
+;; Files and Namespaces
+
+;; Code in Clojure projects is structured into namespaces defined in files.
+;; Usually each file corresponds to one namespace identified by the file's path.
+
+
+;; See how the namespace is called 'training-day', but the file is training_day.clj?
+;; This is intentional.  If a namespace name has a hyphen, the corresponding file
+;; nmae should have an underscore.
+
+
+;; (use 'training-day)
+
+;; What 'use' did was that it looked inside the namespace 'training-day' and
+;; brought with it all the names defined in that namespace. The name 'hai' was one
+;; of them.
+
+
+(defn square [number]
+  "square takes a number and returns the square value"
+  (* number number))
+
+(defn average [num-1 num-2]
+  "takes two number and returns the average"
+  (/ (+ num-1 num-2) 2))
