9ab84420d859b20af0eafefa9fc576d9603c91e4
saleer <eero.t.salminen>
2015-01-16 15:43:33 +0200

    Ready!
    Set go

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,19 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x) )
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b )  2 ) )
+
+; (square 2)
+
+; (average 2 3)
+
+(+ 1 2 (/ 1 2))
+
+
