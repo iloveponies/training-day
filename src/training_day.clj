f9a1d04b44a357af1c33707d601cd83e126b2a7e
unknown <QZI8723@CORPQZI8723A.hca.corpad.net>
2015-02-12 11:53:20 -0600

    Basic clojure ocommands in Training Day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
-(ns training-day)
+(ns training-day) ;;tells you that the code is in the training-day namespace
+                  ;;if the namespace has a -, the file name should have _
 
-(def answer ":(")
+(def answer 42) ;;assign a value of 42 to the variable "answer"
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x)
+)
 
 (defn average [a b]
-  ":(")
+  ( / ( + a b) 2))
