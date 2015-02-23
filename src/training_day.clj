94d29270278cc38a5ccbec9cef0e66c9604c0cbe
Embolic <cjlesh@gmail.com>
2015-02-23 00:37:27 +0000

    Initial commit, tests are passing

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  "Returns the square of a number"
+  [x]
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn 
+  average
+  "Returns the average of two numbers"
+  [a b]
+  (/ (+ a b) 2))
