a664301875a22149424290a8e40e2f09b8bc9f90
Md. Mesbahul Islam <mdislam@cs.helsinki.fi>
2015-03-06 10:35:09 +0200

    training day code submission

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject training-day/training-day "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev
              {:dependencies
               [[midje "1.4.0"]
--- src/training_day.clj
+++ src/training_day.clj
@@ -4,8 +4,12 @@
 
 (def hai "O HAI!")
 
-(defn square [x]
+(defn
+  square
+  [x]
   (* x x))
 
-(defn average [a b]
+(defn
+  average
+  [a b]
   (/ (+ a b) 2))
