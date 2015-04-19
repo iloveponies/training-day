8d92a6a4ff312b63d6361083fb4ebb5cd75a0b17
Takosuke <solidparallel6890@gmail.com>
2015-04-19 03:45:00 +0100

    First training day fix

--- src/training_day.clj
+++ src/training_day.clj
@@ -9,9 +9,3 @@
 
 (defn average [a b]
   (/  (+ a b) 2))
-
-(defn -main
-  [x]
-  (println "me hago pis en el coche de los hombres marranos"))
-
-(println "me hago caca en tu boca de mujer marrana")
--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,6 +1,2 @@
 (ns training-day-test
   (:use iloveponies.tests.training-day))
-(import 'javax.swing.JFrame)
-(def frame (JFrame. "Hello Frame"))
-(.setSize frame 200 200)
-(.setVisible frame true)
