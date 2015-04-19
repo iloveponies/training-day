ce3c7241964c26aa7a0717a2fc8775ae4c1b5cec
Takosuke <solidparallel6890@gmail.com>
2015-04-19 03:41:00 +0100

    First training day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/  (+ a b) 2))
+
+(defn -main
+  [x]
+  (println "me hago pis en el coche de los hombres marranos"))
+
+(println "me hago caca en tu boca de mujer marrana")
