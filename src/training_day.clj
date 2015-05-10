b9c53c2b18b6083f540f23d884a756bd640aa611
Lehtola Paula K <pklehtol@x301-6.cs.helsinki.fi>
2015-05-10 17:04:53 +0300

    jeejee

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
+
+(def perkele "thatcher")
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
