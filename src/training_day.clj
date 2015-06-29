c31dbd049f057f784d2a83c39e1053edcedeed1d
Nicolaus93 <nico.campolongo@gmail.com>
2015-06-29 14:45:31 +0200

    Update training_day.clj

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer "42")
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (str "The result is " (* x x) "!"))
 
 (defn average [a b]
-  ":(")
+  (str "average: " (/ (+ a b) 2))
