e912fbe342d4b0a0a11401b17262ee3655a6ea81
Teemu Salminen <teemujsalminen@gmail.com>
2015-07-02 14:09:48 +0300

    training-day done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,24 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2) )
+
+(def hello (fn [who] (str "Hello, " who "!")))
+
+; (use 'training-day)
+; (square 7)
+; (+ 4 (* 2 3))
+; (+ 3 4 5 6)
+; (get "abrakadabra" 0)
+; ((fn [name] (str "Welcome to Rivendell mr " name)) "Teemu")
+; (hello "Teemu")
+; answer
+; (square 3)
+; (average 2 4)
