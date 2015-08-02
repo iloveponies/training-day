f14396c1ebbd1502eec43fb548ceb3b807b71916
Nabuska <joonaenb@gmail.com>
2015-08-02 11:56:54 +0300

    basics

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,6 +1,8 @@
 
 (ns training-day) ; ns = namespace
 
+(def answer 42)
+
 (defn                                 ; Start a function definition:
   hello                               ; name
   "Gives out personalized greetings." ; a optional docstring
@@ -9,29 +11,13 @@
 
 ;to get the functions doc: write (use 'clojure.repl);... (doc functionName)
 
-(def answer ":(")
-
 (def hai "O HAI!")
-
 (defn square [x]
   (* x x))
 
 
 (defn average [a b]
   (/(+ a b) 2))
+
 (defn hellowho [who]
   (println(str "Hello " who)))
-
-;this is comment
-; 4 + (2*3)
-(println (+ 4 (* 2 3)))
-(println (square 5))
-(println (average 2 9.5))
-(println (str hai " " 2100 "!"))
-(println (take 20(cycle ["fi" "ba"])))
-(println (str "say the n:th letter from 'heppa' " (get "heppa" 2)))
-(hellowho "Joona")
-;Anonymous function
-(def kotoisin (str ((fn [city] (str "Olen kotoisin " city ":sta")) "Helsinki")))
-
-(println (str "Olen kotoisin " kaupunki ":sta"))
