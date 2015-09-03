202bca8ae182ea0638b68b3383baee56a4a84686
Sam Sheffield <sam@sheffinet.org>
2015-09-03 16:40:49 -0400

    Sam Sheffield Training-day

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.0" :exclusions [org.clojure/clojure]]]
+                  :plugins [[lein-midje "3.1.3"]]}})
+
--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
+;namespace definition
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
