78150906946e96ca52c10ab73a4ab768d2727317
pkdahl <pkdahl@gmail.com>
2015-09-30 16:20:28 +0200

    all checks passed

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer (+ 42))
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Returns the square of the argument."
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "Returns the average of the two arguments given."
+  (/ (+ a b) 2))
