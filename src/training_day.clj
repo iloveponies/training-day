28bb9ab8259846e04ae019d29092512c4d3e1e18
Miika Säynevirta <mayse@iki.fi>
2015-10-26 14:59:10 +0200

    message here

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
+
+
+(defn average [a b]
+  (/ (+ a b) 2))
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
-(defn average [a b]
-  ":(")
