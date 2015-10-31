300a18969f8441c28f8200a68a9f52daf7114394
Jeremy <jalee453@gmail.com>
2015-10-31 16:43:02 -0400

    ex1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(use 'training-day)
+
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
