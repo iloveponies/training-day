81c8162f8e04d633dc5b01b2e1f33b20fa20167e
Olli Vistbacka <olli.vistbacka@iki.fi>
2016-02-19 11:59:00 +0200

    Implemented square and average functions.

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  "Multiplies parameter with itself."
+  [x]
+  (* x x))
 
-(defn average [a b]
-  ":(")
+(defn
+  average
+  "Returns the average of its two parameters"
+  [a b]
+  (/ (+ a b) 2))
