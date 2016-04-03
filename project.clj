a650cfabd88e408b82ca72c9c35f390d81cca5ad
cnascone <chris@nascone.net>
2016-04-03 09:16:59 -0400

    Woo, training day done

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
+  :plugins [[cider/cider-nrepl "0.11.0"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
