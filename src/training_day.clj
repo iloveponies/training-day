d241f373406f5cbd8a4c0aae81002bbeaf0b73e1
Toni Laroma <toni.laroma@houston-inc.com>
2016-04-04 23:03:42 +0300

    tweax

--- src/training_day.clj
+++ src/training_day.clj
@@ -2,10 +2,20 @@
 
 (def answer 42)
 
+answer
+
 (def hai "O HAI!")
+
+hai
 
 (defn square [x]
   (* x x))
 
+(square 2) ;=> 4
+(square 3) ;=> 9
+
 (defn average [a b]
   (/ (+ a b) 2))
+
+(average 2 4) ;=> 3
+(average 1 2) ;=> 3/2
