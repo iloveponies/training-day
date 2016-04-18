01140d3fcf17d3cdce5c32bfa1f21f505ada24d4
Uula Ulkuniemi <Uula Ulkuniemi>
2016-04-18 16:13:27 +0300

    Tehtävien tarkistus

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square "Square root" [x]
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
