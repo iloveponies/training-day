ae7a80f4c034265732ca7902608d997c4858f8bb
Edson Sousa <edsonrodrigosousa@gmail.com>
2016-04-18 17:40:19 -0300

    final commit

--- src/training_day.clj
+++ src/training_day.clj
@@ -7,4 +7,6 @@
 (defn square [x]
  (* x x))
 
+(defn square2 [x] (* x x x))
+
 (defn average "teste docs" [a b] (/ (+ a b) 2))
---
+++ src/untitled.clj
@@ -0,0 +1,22 @@
+(+ 2 3)
+(use 'training-day)
+(use 'clojure.repl)
+
+(square 7)
+(str "dfd " 'sdfdf' 44545)
+(+ 4 (* 2 3))
+(+ 3 4 5 6)
++
+(get "Edson" 2)
+(get "abrakadabra" 0)
+((fn [who] (str "Hello, " who "!")) "Chico")
+(def hello (fn [who] (str "Hello, " who "!")))
+(hello "ddsds")
+answer
+(average 50 70)
+(doc +)
+(doc average)
+hai
+
+(boolean false)
+(= "foo" "foo")
