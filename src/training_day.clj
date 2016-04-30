a935202276e456e82f15514221d2621dc977b0ad
Joonas Pesonen <joonas.pesonen@csc.fi>
2016-04-30 15:52:44 +0300

    first exercises

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,15 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  "Get square of a number"
+  [x]
+  (* x x)
+  )
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
