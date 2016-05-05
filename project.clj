712c278b464789bcdcc4475b394bee8889386d3b
Tapio Jaakkola <tapio.jaakkola@gofore.com>
2016-05-05 13:13:43 +0300

    first assingments

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
+;; Anything you type in here will be executed
+;; immediately with the results shown on the
+;; right.
+
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
