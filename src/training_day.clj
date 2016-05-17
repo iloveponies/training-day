4388491c7eb19ad1cbb2bd0af71477696e3795cd
pzheiska <pyry.heiskanen@gmail.com>
2016-05-17 15:11:44 +0300

    jee jee

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer (int 42))
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (int (* x x)))
+
+(square 2)
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+(average 2 1)
+
+
+
