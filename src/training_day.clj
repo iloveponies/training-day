984488e6a3661f4e35febe58df42223ebdb5af55
tamamu <tamamu.1r1s@gmail.com>
2016-06-15 23:25:32 +0900

    first training

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
+(use 'training-day)
+
 (def answer ":(")
 
 (def hai "O HAI!")
 
+(def answer 42)
+
 (defn square [x]
-  ":(")
+  ":("
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  ":("
+  (/ (+ a b) 2))
