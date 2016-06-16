ca34ed7a2f97fae11529dd98cef5697b614d3d2e
alex argunov <sajkoooo@gmail.com>
2016-06-16 22:34:04 +0300

    g add -A

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+   :dependencies [[org.clojure/clojure "1.7.0"]
+                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
+   :profiles {:dev {:dependencies [[midje "1.7.0"]]}}
+   :plugins [[lein-midje "3.2-RC4"]])
+
