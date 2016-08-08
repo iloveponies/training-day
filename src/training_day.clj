3f8665016819c584368516d8bff9893a86a11a7a
WON SEOB SEO <pghoou@hotmail.com>
2016-08-08 17:23:25 +0300

    Finish traning day

--- src/training_day.clj
+++ src/training_day.clj
@@ -5,7 +5,29 @@
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
++
+
+(get "Clojure" 2)
+
+((fn [who] (str "Hello, " who "!")) "Jani")
+
+(def hello "Gives out personalized greetings." (fn [name] (str "Welcome to Rivendell mr " name)))
+
+(hello "handsome")
+(hello "beautiful")
+(def answer 42)
+
+(use 'clojure.repl)
+(use 'training-day)
+
+(doc +)
+(doc hello)
+(+)
+
+(use 'training-day)
+hai
