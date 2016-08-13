927bdfeff0c151d52ba16c7c2d18b1a238bb2d92
Robert Valta <robert.j.valta@accenture.com>
2016-08-13 16:48:34 +0300

    first 7 exercises done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(def hello (fn [who] (str "asd, " who "!")))
+
+(println hai)
+(square 2)
