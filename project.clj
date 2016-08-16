2470b90c976c8d2e842358d6cac019803870dfbd
Darius Aliulis <darius.aliulis@gmail.com>
2016-08-17 02:57:14 +0300

    ... cli fix

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]
-                             [refactor-nrepl "2.2.0"]
-                             [cider/cider-nrepl "0.11.0"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
