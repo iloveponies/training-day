2eabe37ee64bafaa7d9cb05eae5eb7f846bc40aa
Darius Aliulis <darius.aliulis@gmail.com>
2016-08-17 02:46:56 +0300

    training-day passed

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]
+                             [refactor-nrepl "2.2.0"]
+                             [cider/cider-nrepl "0.11.0"]]}})
