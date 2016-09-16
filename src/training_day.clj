3523e15b1bf40a6d9d36701e7575f9083355436d
Julius <julius@uusinarkaus.com>
2016-09-16 16:51:52 +0300

    ekat tehtävat

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,52 @@
 (ns training-day)
 
-(def answer ":(")
+(+ (* 2 3) 4)
+
+(+ 3 4 5 6)
+
+(get "abrakadabra" 0)
+
+((fn [who] (str "Hello, " who "!")) "Jani")
+
+((fn [name] (str "Welcome to Rivendell mr " name)) "Julius")
+
+(def hello (fn [who] (str "Hello, " who "!")))
+
+(hello "beautiful")
+
+
+
+
+
+
+
+
+
+(def answer 42)
+
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
+
+
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
