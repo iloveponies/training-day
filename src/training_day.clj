c21a30ff270ee163e3d4c086411008809be4166b
SDMoonlight <sdmoonlight@gmx.com>
2016-10-16 20:22:22 +1300

    Results?

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
+(use 'training-day)
 
-(def answer ":(")
+(def answer 42)
+
+answer
 
 (def hai "O HAI!")
-
 (defn square [x]
-  ":(")
+  (* x x))
+(square 2)
+(square 3)
 
-(defn average [a b]
-  ":(")
+(defn average [a b] (/ (+ a b) 2))
+(average 2 4)
+(average 1 2)
+
