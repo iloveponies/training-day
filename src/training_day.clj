dd0bdcb6177681bb7da4f84bc1c3fb7513a92994
drew.okane <drew.okane@climate.com>
2016-10-29 14:22:55 -0700

    Fixed training-day tests

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Returns the square of a number"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "Returns the average of two numbers"
+  (/ (+ a b) 2))
