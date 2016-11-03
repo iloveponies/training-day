4c52354c081cbb02e8d1db86ff647fe40aa8a5c5
Piyush Shah <piyush_shah@apple.com>
2016-11-02 22:22:12 -0700

    working square /average

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+   (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+;;(+ 1 (* 2 3) 4)
+
+;;(/    (+ 4 10) 2 )
+
+
+
