d3e2400d50eab91baec1e9a84eaa5e0beb6a0fd9
Sami Hirvonen <Sami Hirvonen>
2016-11-08 21:14:35 +0200

    tehtavat1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,11 @@
 (ns training-day)
 
-(def answer "42")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  (str (* x x)))
+  (* x x))
 
 (defn average [a b]
-  (str (/ (+ a b) 2)))
+  (/ (+ a b) 2))
