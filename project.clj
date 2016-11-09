287e3552d97ee97dc79f6268c1967eb8fad84239
Adomas Jatužis <adomixaszvers@gmail.com>
2016-11-10 01:08:45 +0200

    message here

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
