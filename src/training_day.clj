cbb3a15eee91a89968e6ed3523783b67681d8538
Sarah Hamdi <s.hamdi@gmail.com>
2016-11-21 16:34:50 -0500

    exercise 1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
+(defn add [a b]
+  (+ a b))
+
+(add 5 6)
+
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
