67361806cdf8606b8e2c807aaa17cb626ea12358
Summer Babe <theopenmind21@gmail.com>
2017-01-10 16:56:01 +0200

    i ❤ ponies 2

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(
+  defn square
+  "Computes a square of given number!"
+  [tale]
+  (* tale tale))
 
-(defn average [a b]
-  ":(")
+(
+  defn average
+  "Computes an average of two given numbers!"
+  [tale fairy]
+  (/ (+ fairy tale) 2 ))
