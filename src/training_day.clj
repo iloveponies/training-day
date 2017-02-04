2a19c28fc1ad4ff6ac20163ed61076492ad5124d
Lucas Mancini <mancini.lucas@gmail.com>
2017-02-04 16:58:10 -0300

    Implementing functions to make tests pass.

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,31 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(+ 4 (* 2 3))
+
+(+ (+ (+ 3 0) 5) 6)
+
+(- 3)
+
+(get "abrakadabra" 0)
+
+(fn [who] (str "Hello, " who "!"))
+((fn [who] (str "Hello, " who "!")) "Lucas")
+
+((fn [name] (str "Welcome to Rivendell mr " name)) "Lucas")
+
+(defn hello
+ "Personalized string settings"
+ [who]
+ (str "Hello, " who "!"))
+
+(hello "beautiful")
