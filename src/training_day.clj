28b7f8ad8a5e36515a0b434f1faeba767eb288b4
Lucas Mancini <mancini.lucas@gmail.com>
2017-02-04 16:59:01 -0300

    Removing unused code.

--- src/training_day.clj
+++ src/training_day.clj
@@ -9,23 +9,3 @@
 
 (defn average [a b]
   (/ (+ a b) 2))
-
-(+ 4 (* 2 3))
-
-(+ (+ (+ 3 0) 5) 6)
-
-(- 3)
-
-(get "abrakadabra" 0)
-
-(fn [who] (str "Hello, " who "!"))
-((fn [who] (str "Hello, " who "!")) "Lucas")
-
-((fn [name] (str "Welcome to Rivendell mr " name)) "Lucas")
-
-(defn hello
- "Personalized string settings"
- [who]
- (str "Hello, " who "!"))
-
-(hello "beautiful")
