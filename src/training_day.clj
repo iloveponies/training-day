9aee49b9a51c7f6037d36f2f67e0acb042494497
Ollipekka Väänänen <ollivaan@dhcp-asv-101.helsinki.fi>
2017-02-13 18:44:03 +0200

    ekat tehtävät

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,12 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [a]
+  (* a a))
 
 (defn average [a b]
-  ":(")
+  (/(+ a b) 2))
+
