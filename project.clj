f3633a154a7dcc24cd85c695a8688bc558709f99
Atte Lassila <atte.lassila@cs.helsinki.fi>
2017-02-20 23:20:17 +0200

    exercises done

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
+                 [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]
+                 [proto-repl "0.3.1"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
