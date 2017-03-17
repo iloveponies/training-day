38f053cb97edc8dcd75770e808b604b61bc29d49
LeroyKim <LeroyKim92@gmail.com>
2017-03-17 16:51:22 +0900

    answer commit 0

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,15 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer
+  (fn []
+    42))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn
+  square
+  [x]
+  (* x x))
 
 (defn average [a b]
-  ":(")
+   (/ (+ a b) 2))
