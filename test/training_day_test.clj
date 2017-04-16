ef8e27d69367f4801921f3fbf1b4f64ad5781e70
Sami Nieminen <sami.nieminen@solita.fi>
2017-04-16 17:18:21 +0300

    Implemented

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,2 +1,21 @@
 (ns training-day-test
-  (:use iloveponies.tests.training-day))
+  (:use iloveponies.tests.training-day)
+  (:require [training-day :refer :all]
+            [midje.sweet :refer :all]))
+
+(square 2)
+
+(facts "square"
+       (square 2) => 4
+       (square 3) => 9)
+
+(average 1 3)
+
+(facts "average"
+       (average 1 3) => 2
+       (average 5 5) => 5)
+
+answer
+
+(facts "answer"
+       answer => 42)
