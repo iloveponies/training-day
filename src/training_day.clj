b53d9f2c50da3c5644907bcab591cf7cf1b2def0
JaneAdrift <janeadrift@foxmail.com>
2017-04-23 23:16:23 +0800

    nice job! 2017/4/23 The first

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,30 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "square a number" (* x x))
 
 (defn average [a b]
-  ":(")
+  "average two number" (/(+ a b) 2))
+
+(+ 4 (* 2 3))
+(+ 3 4 5 6)
++
+(get "abrakadabra" 0)
+((fn [who] (str "Hello, " who "!")) "Jani")
+((fn [name] (str "Welcome to Rivendell mr " name)) "Adrift")
+(def hello (fn [who] (str "Hello, " who "!")))
+(defn
+  hello
+  "Gives out personalized greetings."
+  [who]
+  (str "Hello, " who "!"))
+(use 'clojure.repl)
+(doc +)
+(doc hello);(user/clojuredocs min)
+(use 'training-day)
+hai
+answer
