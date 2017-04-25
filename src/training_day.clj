d67c3563838fd006cf967083bfa09526bdf5f339
Väinö Niemi <vaino.a.niemi@gmail.com>
2017-04-25 18:41:02 +0300

    Exercises 5, 6, 7 done

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)                                             ;=> 42
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn                                                       ; Start a function definition
+  square                                                    ; name
+  "Calculates the square of given value [x]"                ; an optional docstring
+  [x]                                                       ; parameters inside brackets
+  (* x x))                                                  ; body
 
-(defn average [a b]
-  ":(")
+(defn
+  average
+  "Calculates the average of the given two parameters [x, y]"
+  [x y]
+  (/ (+ x y) 2))
