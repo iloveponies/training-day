be561cbb6e9e2ff2a6af254a26f6aeb1465f14c6
nlaitine <niko.laitinen@nitor.fi>
2017-04-25 22:46:02 +0300

    custom .gitignores removed

---
+++ src/training_day.clj
@@ -0,0 +1,15 @@
+(ns training-day)
+
+(def answer (int 42))
+
+(def hai "O HAI!")
+
+(defn square
+  "Prints square"
+  [x]
+  (* x x))
+
+(defn average
+  "Prints average"
+  [a b]
+  (/ (+ a b) 2))
