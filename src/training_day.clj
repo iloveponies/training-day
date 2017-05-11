c62da948a89f0bc1f89c2803ebee13aad0a45b9e
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2017-05-11 10:30:12 +0300

    training day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,4 +1,6 @@
-(ns training-day)
+(ns training-day
+  (:use [clojure.repl]))
+
 
 (def answer 42)
 
@@ -9,3 +11,34 @@
 
 (defn average [a b]
   (/ (+ a b) 2))
+
+;"(2*3)+4"
+(+ (* 2 3) 4)
+
+;"3+4+5+63+4+5+6"
+(+ 3 4 5 6 3 4 5 6)
+
+(get "Clojure" 0)
+
+;; "Write a Clojure expression in the REPL that, using get, gets the first character in the string 'abrakadabra'."
+(get "abrakadabra" 0)
+
+(def hello (fn [who] (str "Hello, " who "!")))
+
+(hello "Topi")
+
+(def nimi "Topi")
+
+;; hello
+(defn hello [who]
+  (str "Hello, " who "!"))
+
+(hello nimi)
+
+(defn foo [stuff]
+  (take 5 (cycle stuff)))
+
+(foo ["1", "2", "3"])
+
+(defn foo [stuff]
+  (take 5 (cycle stuff)))
