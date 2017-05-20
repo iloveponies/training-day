bb6ae0fe9d94f6f5259b6062d105947d075eee1d
Jenni Linteri <jenni.linteri@gmail.com>
2017-05-20 16:46:46 +0300

    first clojure training section

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,13 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer(+ 30 12))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square "Multiplies itself" [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(defn
+  average
+  "Returns average of parameters"
+  [a b]
+  (/ (+ a b) 2))
