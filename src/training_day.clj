c9cdf0b540c840afc83c11688956fdc8788ad65c
Arto Jussilainen <artojussilainen@gmail.com>
2017-06-10 19:33:32 +0300

    exercises 1-7

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,9 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer ((fn [num](+ 2 num)) 40))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average [a b] (/ ( + a b) 2))
