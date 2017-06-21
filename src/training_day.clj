b7a05a2042aa6b6c46cda9bcc4e23d9182225015
Pauliina Karell <Pauliina.Karell@enfogroup.com>
2017-06-21 10:00:55 +0300

    all tests passed

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,12 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "Returns num squared"
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
