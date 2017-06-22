749e38e80e1398514c355574adbfe504f5341b92
Peter Hayes <peterhayes1@hotmail.com>
2017-06-22 21:36:16 +0100

    Code tidy

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,22 @@
 (ns training-day)
 
+;; Ex 5
+;; Give a name answer to the answer to life the universe and everything. This is the first exercise in which you need to modify the file src/training_day.clj.
+;; Remember to run the tests with lein midje.
+
 (def answer 42)
 
 (def hai "O HAI!")
 
+;; Ex 6
+;; Write the function (square x) that takes a number as a parameter and multiplies it with itself.
+
 (defn square [x]
   (* x x))
 
+;; Ex 7
+;; Write the function (average a b), which returns the average of its two parameters:
+
 (defn average [a b]
   (/ (+ a b) 2))
+
