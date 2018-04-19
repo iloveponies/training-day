db526d0226cfa2ddf1299c9499b3ca5a02410c0f
Gautam Roy <gautamr@nrifintech.com>
2018-04-19 10:14:35 +0530

    upgrade to Clojure version 1.9.0 and midje 1.9.1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.9.1"]]
+                   :plugins [[lein-midje "3.2.1"]]}})
