c01d32cf85a74d10cc8aa8aab4408e4c0756c82e
Marcos Muniz <mmuniz@groupon.com>
2018-04-23 20:52:25 -0500

    first clojure code

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer "Gives the universal answer"  42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(average 2 3)
+
+
+
+(use 'training-day)
+hai
