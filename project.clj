2d2484d1dfd193ede2625cbfe55cd0a05b709485
Matti Uusitalo <matti.uusitalo@solita.fi>
2019-02-13 12:00:05 +0200

    Training day succeeds

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :managed-dependencies [[midje "1.9.6"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+  :profiles {:dev {:depdendencies [[midje "1.9.6"]]
+                   :plugins [[lein-midje "3.2.1"]
+                             [lein-ancient "0.6.15"]]}})
