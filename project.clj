06b7ae71fd3d59b02684df6f429c3e02e2d0b1c9
Matti Uusitalo <matti.uusitalo@solita.fi>
2019-02-20 11:33:39 +0200

    Corrected dependencies so I can run this

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :managed-dependencies [[midje "1.9.6"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:depdendencies [[midje "1.9.6"]]
+  :profiles {:dev {:dependencies [[midje "1.9.6"]]
                    :plugins [[lein-midje "3.2.1"]
                              [lein-ancient "0.6.15"]]}})
