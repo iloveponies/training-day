19f23863669f447c9fabd7a86a6d35e4c82a2d31
Brian McClimens <brian.mcclimens@nrl.navy.mil>
2019-03-19 12:14:28 -0400

    message here

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject training-day "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [midje "1.9.6" :scope "test"]
+   [marick/suchwow "6.0.2" :scope "test" :exclusions [[org.clojure/clojure] [org.clojure/clojurescript]]]
+     [com.rpl/specter "0.13.0" :scope "test" :exclusions [[org.clojure/clojure] [org.clojure/clojurescript]]]
                  [iloveponies.tests/training-day "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+
+  )
