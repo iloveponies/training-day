562282476b30a437343578d3ef093c7f036d1164
adm in <testetst>
2019-08-20 08:03:10 +0300

    success

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
+(defn printhello [] 
+  (println hai))
+
 (defn square [x]
-  ":(")
+  (* x x))
+  ; (reduce * (repeat 2 x)))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
