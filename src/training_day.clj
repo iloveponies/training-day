6f0f56b7fa298341ae7b03bb83b4a87475550196
surovtsev_vrn <alexandr.surovtsev.vrn@yandex.ru>
2021-01-01 02:58:45 +0300

    done chapter 1

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,18 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
+(defn           ; Start a function definition:
+  hello         ; name
+  "Gives out personalized greetings." ; a optional docstring
+  [who]                               ; parameters inside brackets
+  (str "Hello, " who "!"))            ; body
+
