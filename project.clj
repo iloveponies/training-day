7e8696b68e09ee53f885d86c1ae4b9f146137f59
Taci Öztürk <taciozturk@gmail.com>
2023-05-12 22:40:35 +0300

    training-day-completed

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject training-day "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [iloveponies.tests/training-day "0.2.0-SNAPSHOT"]]
+                 [iloveponies.tests/training-day "0.2.0-SNAPSHOT"]
+                 [proto-repl "0.3.1"]]
   :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
