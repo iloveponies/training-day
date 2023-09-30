474c641005e713df9e488763aaacf3ddf508465d
Marios <mariosnic23@gmail.com>
2023-09-30 09:33:59 +0300

    solutions for first chapter

--- test/training_day_test.clj
+++ test/training_day_test.clj
@@ -1,2 +1,17 @@
 (ns training-day-test
-  (:use iloveponies.tests.training-day))
+  (:use midje.sweet
+        training-day))
+
+(facts "answer" {:exercise 5
+                 :points 1}
+       answer => 42)
+
+(facts "square" {:exercise 6
+                 :points 1}
+       (square 2) => 4
+       (square 3) => 9)
+
+(facts "average" {:exercise 7
+                  :points 1}
+       (average 2 4) => 3
+       (average 1 2) => 3/2)
